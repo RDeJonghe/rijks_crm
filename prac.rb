1aa3e5efe6520634cee166296e89e8577131e5e8
RDeJonghe <rdejonghe@hotmail.com>
2022-03-28 11:32:46 -0400

    chng

--- prac.rb
+++ prac.rb
@@ -1,39 +1,33 @@
 require 'httparty'
 require 'json'
 
-class VanGogh
-  API_CALL = HTTParty.get('https://www.rijksmuseum.nl/api/en/collection?key=ME1aaDBz&involvedMaker=Vincent+van+Gogh&imgonly=True&p=0-9999&s=chronologic')
+class Bosch
+  BOSCH_API_CALL = HTTParty.get('https://www.rijksmuseum.nl/api/en/collection?key=ME1aaDBz&involvedMaker=Jheronimus+Bosch&imgonly=True&p=0-9999&s=chronologic')
 
-  VAN_GOGH_ART_OBJECTS = JSON.parse(API_CALL.body)["artObjects"]
-
-  def self.api_call
-    response = HTTParty.get('https://www.rijksmuseum.nl/api/en/collection?key=ME1aaDBz&involvedMaker=Vincent+van+Gogh&imgonly=True&p=0-9999&s=chronologic')
-    
-    response 
-  end
+  BOSCH_ART_OBJECTS = JSON.parse(BOSCH_API_CALL.body)["artObjects"]
 
   def titles
-    VAN_GOGH_ART_OBJECTS.each_with_object([]) do |art_obj, results|
+    BOSCH_ART_OBJECTS.each_with_object([]) do |art_obj, results|
       results << { title: art_obj["title"], id: art_obj["id"] }
     end
   end
 
   def image(id)
-    VAN_GOGH_ART_OBJECTS.select do |art_obj|
+    BOSCH_ART_OBJECTS.select do |art_obj|
       art_obj["id"] == id
     end
     .first["webImage"]["url"]
   end
 
   def work_title(id)
-    VAN_GOGH_ART_OBJECTS.select do |art_obj|
+    BOSCH_ART_OBJECTS.select do |art_obj|
       art_obj["id"] == id
     end
     .first["title"]
   end
 
   def produced(id)
-    VAN_GOGH_ART_OBJECTS.select do |art_obj|
+    BOSCH_ART_OBJECTS.select do |art_obj|
       art_obj["id"] == id
     end
     .first["longTitle"]
@@ -42,15 +36,60 @@ class VanGogh
   end
 end
 
+bosch = Bosch.new
 
-ARTISTS_OBJ = {
-  van_gogh: VanGogh.api_call
-}
+p bosch.titles
+
+# class VanGogh
+#   API_CALL = HTTParty.get('https://www.rijksmuseum.nl/api/en/collection?key=ME1aaDBz&involvedMaker=Vincent+van+Gogh&imgonly=True&p=0-9999&s=chronologic')
+
+#   VAN_GOGH_ART_OBJECTS = JSON.parse(API_CALL.body)["artObjects"]
+
+#   def self.api_call
+#     response = HTTParty.get('https://www.rijksmuseum.nl/api/en/collection?key=ME1aaDBz&involvedMaker=Vincent+van+Gogh&imgonly=True&p=0-9999&s=chronologic')
+    
+#     response 
+#   end
+
+#   def titles
+#     VAN_GOGH_ART_OBJECTS.each_with_object([]) do |art_obj, results|
+#       results << { title: art_obj["title"], id: art_obj["id"] }
+#     end
+#   end
+
+#   def image(id)
+#     VAN_GOGH_ART_OBJECTS.select do |art_obj|
+#       art_obj["id"] == id
+#     end
+#     .first["webImage"]["url"]
+#   end
+
+#   def work_title(id)
+#     VAN_GOGH_ART_OBJECTS.select do |art_obj|
+#       art_obj["id"] == id
+#     end
+#     .first["title"]
+#   end
+
+#   def produced(id)
+#     VAN_GOGH_ART_OBJECTS.select do |art_obj|
+#       art_obj["id"] == id
+#     end
+#     .first["longTitle"]
+#     .split(", ")
+#     .last
+#   end
+# end
 
 
-result = ARTISTS_OBJ[:van_gogh]
-# p result
+# ARTISTS_OBJ = {
+#   van_gogh: VanGogh.api_call
+# }
 
-vg = VanGogh.new.titles
 
-p vg
+# result = ARTISTS_OBJ[:van_gogh]
+# # p result
+
+# vg = VanGogh.new.titles
+
+# p vg
