adfd4807d8352e97c4db4cf42967a1afba15884d
RDeJonghe <rdejonghe@hotmail.com>
2022-03-30 16:19:31 -0400

    test suite

--- test/rijks_crm_test.rb
+++ test/rijks_crm_test.rb
@@ -25,6 +25,21 @@ class RijksCrmTest < Minitest::Test
     Sinatra::Application
   end
 
+  # Likely need a set up so that info can be posted so there are clients and interactions to test for
+  # add all the contact info
+
+  def setup
+    post '/clients', client_first: "Joe", client_last: "Jones", email: "jj@gmail.com", phone: "333-444-5555", street: "123 Main St", city: "Springfield", state: "KS", postal: "45654"
+    post '/interactions', date: "03/30/2022", full_name: "Joe Jones", type: "Email", comments: "Sent message"
+    post '/admin/signin', username: "Ryan", password: "orange"
+  end
+
+  # This is added so a session can be accessed in testing. You'll see session[:key] in the tests accessing this
+
+  def session
+    last_request.env["rack.session"]
+  end
+
   def test_home_redirect
     skip
     get '/'
@@ -170,13 +185,28 @@ class RijksCrmTest < Minitest::Test
   # This test is similar to above test but it will first create a client. With an existing client it will then create an interaction successfully. This will test that the listing of interactions includes this new interaction (but this is not the page for the actual interaction details)
 
   def test_post_interactions_with_created_client
+
     skip
-    post '/clients', client_num: "99999", client_first: "Joe", client_last: "Jones"
-    post '/interactions', date: "03/30/2022", full_name: "Joe Jones", type: "Email", comments: "Sent message"
+    # THIS WAS ALL CREATED WITH SET UP, SO CAN JUST ASSERT
+    # post '/clients', client_first: "Joe", client_last: "Jones"
+    # post '/interactions', date: "03/30/2022", full_name: "Joe Jones", type: "Email", comments: "Sent message"
     get '/interactions'
     assert_includes last_response.body, "Joe Jones"
     assert_includes last_response.body, "03/30/2022"
     assert_includes last_response.body, "Email"
+  end
+
+  # SAME TEST AS ABOVE BUT WITH A SESSION, USES SET UP, CAN JUST USE THIS TEST
+
+  def test_post_interactions_with_created_client
+
+    skip
+
+    get '/interactions'
+    assert_equal "Joe Jones", session[:interactions][0][:client_full]
+    assert_equal "03/30/2022", session[:interactions][0][:date]
+    assert_equal "Email", session[:interactions][0][:type]
+    assert_equal "Sent message", session[:interactions][0][:comments]
   end
 
   # This test is to test a created client. The client is created and the client page list is checked to make sure they appear on it with the format of first, last.
@@ -187,9 +217,66 @@ class RijksCrmTest < Minitest::Test
     get '/clients'
     assert_includes last_response.body, "Jones, Joe"
   end
+
+  # same test as above but with a session. The client id is generated randomly, so here we are just testing the size of it.
+  def test_post_created_client_with_session
+    skip
+    # THIS WAS JUST ALL SET UP WITH THE SETUP METHOD, NO NEED TO DO IT HERE, JUST ASSERT HERE
+    # post '/clients', client_first: "Joe", client_last: "Jones"
+
+    assert_equal 20, session[:clients][0][:client_num].size
+    assert_equal "Joe", session[:clients][0][:client_first]
+    assert_equal "Jones", session[:clients][0][:client_last]
+  end
+
+  # Testing the info page for a client, the client number is gotten from the session and put in the route. Test checks to see if the info that was entered in the set up is displayed on the page.
+
+  def test_client_info_page
+    skip
+
+    client_num = session[:clients][0][:client_num]
+
+    get "/clients/#{client_num}"
+
+    assert_includes last_response.body, "Joe Jones"
+    assert_includes last_response.body, "jj@gmail.com"
+    assert_includes last_response.body, "333-444-5555"
+    assert_includes last_response.body, "123 Main St"
+    assert_includes last_response.body, "Springfield"
+    assert_includes last_response.body, "KS"
+    assert_includes last_response.body, "45654"
+  end
+
+  # testing the interaction page for a specific interaction to make sure it shows the info. Similar to the test above but to test the interaction page instead of the client page
+
+  def test_interaction_info_page
+    skip
+    interaction_id = session[:interactions][0][:id]
+
+    get "/interactions/#{interaction_id}"
+
+    assert_includes last_response.body, "03/30/2022"
+    assert_includes last_response.body, "Joe Jones"
+    assert_includes last_response.body, "Email"
+    assert_includes last_response.body, "Sent message"
+  end
+
+  # tests that an interaction has been deleted. the setup method will set up an interaction. the post method here will delete it. the assertion is checking that the size of the interactions array is 0 since the only interaction will have been deleted
+
+  def test_delete_interaction
+    # skip
+    interaction_id = session[:interactions][0][:id]
+
+    post "/interactions/#{interaction_id}/destroy"
+
+    assert_equal 0, session[:interactions].size
+  end
+
+  # tests that the sign in works from the setup
+
+  def test_signin_working
+    assert_equal "Ryan", session[:username]
+  end
 end
-
-
-
 
 
